aad4b4a71578826c2f6fa3fdc8915109f6840ea6
Luke <Lukewizzle@gmail.com>
2016-03-25 16:44:12 -0400

    Animation now works on key press

--- core/src/com/mygdx/game/GamIntoTheWoods.java
+++ core/src/com/mygdx/game/GamIntoTheWoods.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -17,52 +18,64 @@ import java.awt.event.KeyListener;
 
 
 public class GamIntoTheWoods extends Game {
-	SpriteBatch batch;
-	Texture img;
-	Image imgHero;
-	Sprite sprHero;
 	TextureAtlas taHero;
-	TextureRegion trCurrentFrame, trLeft[], trRight[];
-	Texture txCurrentFrame;
-	Animation aniLeft, aniRight;
+	TextureRegion trCurrentFrame, trLeft[], trRight[],trUp[],trDown[];
+	Animation aniLeft, aniRight,aniUp,aniDown;
 	float fStateTime;
 	SpriteBatch spriteBatch;
+	Texture Background;
+	float HeroX,HeroY,HeroSpeed=50f;
 
 	
 	@Override
 	public void create () {
-		//batch = new SpriteBatch();
-		///img = new Texture("badlogic.jpg");
-		//setScreen(new ScrLaz(this));
-		imgHero = new Image();
-		sprHero = new Sprite();
-		System.out.println("Actor made************************************************************************");
+		Background= new Texture(Gdx.files.internal("lostwoods2.jpg"));
 		taHero = new TextureAtlas("Heroinepacked.pack");
 		trCurrentFrame = new TextureRegion();
 
-
 		trLeft = new TextureRegion[3];
 		trRight = new TextureRegion[3];
+		trUp = new TextureRegion[3];
+		trDown = new TextureRegion[3];
 		for (int i = 0; i < 3; i++) {
 			trLeft[i] = taHero.findRegion("Left" + (i + 1));
 			trRight[i] = taHero.findRegion("Right" + (i + 1));
+			trUp[i] = taHero.findRegion("Up" + (i + 1));
+			trDown[i] = taHero.findRegion("Down" + (i + 1));
 		}
-
-		aniLeft = new Animation(0.15f, trLeft);
-		//aniRight = new Animation(0.15f, trRight);
-		trCurrentFrame = trRight[0];
-		fStateTime = 0f;
-		//bRight = true
+		aniRight= new Animation(1f/4,trRight);
+		aniLeft= new Animation(1f/4,trLeft);
+		aniUp= new Animation(1f/4,trUp);
+		aniDown= new Animation(1f/4,trDown);
+		
 		spriteBatch = new SpriteBatch();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		trCurrentFrame= aniDown.getKeyFrame(0);
 		fStateTime += Gdx.graphics.getDeltaTime();
-		trCurrentFrame = aniLeft.getKeyFrame(fStateTime, true);
+		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) {
+			HeroX -= Gdx.graphics.getDeltaTime() * HeroSpeed;
+			trCurrentFrame = aniLeft.getKeyFrame(0+fStateTime,true);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
+			HeroX += Gdx.graphics.getDeltaTime() * HeroSpeed;
+			trCurrentFrame = aniRight.getKeyFrame(0+fStateTime,true);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_UP)) {
+			HeroY += Gdx.graphics.getDeltaTime() * HeroSpeed;
+			trCurrentFrame = aniUp.getKeyFrame(0+fStateTime,true);
+
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN)) {
+			HeroY -= Gdx.graphics.getDeltaTime() * HeroSpeed;
+			trCurrentFrame = aniDown.getKeyFrame(0+fStateTime,true);
+		}
 		spriteBatch.begin();
-		spriteBatch.draw(trCurrentFrame, 50, 50);
+		spriteBatch.draw(Background,0,0);
+		spriteBatch.draw(trCurrentFrame, (int)HeroX, (int)HeroY);
 		spriteBatch.end();
 	}
 }
