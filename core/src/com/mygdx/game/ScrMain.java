933202fe1d50c027089b41350c20c956ec6267ff
michelle <mpuppy13@gmail.com>
2016-03-29 10:30:37 -0400

    Button appears

--- core/src/com/mygdx/game/ScrMain.java
+++ core/src/com/mygdx/game/ScrMain.java
@@ -3,7 +3,10 @@ package com.mygdx.game;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.Gdx;
@@ -17,15 +20,17 @@ import sun.java2d.ScreenUpdateManager;
  * Created by michelle on 3/22/2016.
  */
  public class ScrMain implements Screen {
-    GamIntoTheWoods gamIntoTheWoods;
-    TextButton tbGame;
+    //GamIntoTheWoods gamIntoTheWoods;
+    //TextButton tbGame;
     Stage stage;
+    SpriteBatch batch;
     Skin skin = new Skin();
     //TbsMenu tbsMenu;
 
     public ScrMain(GamIntoTheWoods gamIntoTheWoods) {
     }
     public void create() {
+        batch = new SpriteBatch();
         stage = new Stage();
         //tbsMenu = new TbsMenu();
         //tbGame = new TbMenu("Game", tbsMenu);
@@ -52,10 +57,14 @@ import sun.java2d.ScreenUpdateManager;
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
         Gdx.gl.glClearColor(1,1,1,1);
-//        if (tbGame.isPressed()) { //When you click the Play Button, it sets the screen to the Play Screen, and updates.
-//            gamIntoTheWoods.currentState = GamIntoTheWoods.GameState.GAME;
-//            gamIntoTheWoods.updateState();
-//        }
+        batch.begin();
+
+        batch.end();
+        stage.draw();
+       /*if (button.isPressed()) { //When you click the Play Button, it sets the screen to the Play Screen, and updates.
+           gamIntoTheWoods.currentState = GamIntoTheWoods.GameState.GAME;
+           gamIntoTheWoods.updateState();
+        }*/
     }
 
     @Override
